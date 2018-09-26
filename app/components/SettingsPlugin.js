87c65c03dfa49f6b7828e6f918d2482f1fbd88b6
Xzandro <mail@xzandro.de>
2018-09-26 16:22:19 +0200

    Added support for other Input fields in settings

--- app/components/SettingsPlugin.js
+++ app/components/SettingsPlugin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Checkbox, Form } from 'semantic-ui-react';
+import { Checkbox, Form, Input } from 'semantic-ui-react';
 import SettingsItem from './SettingsItem';
 
 const { remote } = require('electron');
@@ -7,17 +7,33 @@ const { remote } = require('electron');
 let config = remote.getGlobal('config');
 
 class SettingsPlugin extends React.Component {
+  constructor() {
+    super();
+    this.components = {
+      checkbox: Checkbox,
+      input: Input
+    };
+  }
+
   render() {
-    const pluginConfig = Object.keys(config.Config.Plugins[this.props.pluginName]).map((key, i) => (
-      <Form.Field key={i}>
-        <SettingsItem
-          section="Plugins"
-          pluginName={this.props.pluginName}
-          setting={key}
-          Input={<Checkbox />}
-        />
-      </Form.Field>
-    ));
+    const pluginConfig = Object.keys(config.Config.Plugins[this.props.pluginName]).map((key, i) => {
+      let inputType = 'checkbox';
+      const configDetails = config.ConfigDetails.Plugins[this.props.pluginName];
+      if (configDetails && configDetails[key] && configDetails[key].type) {
+        inputType = config.ConfigDetails.Plugins[this.props.pluginName][key].type;
+      }
+      const InputComponent = this.components[inputType];
+      return (
+        <Form.Field key={i}>
+          <SettingsItem
+            section="Plugins"
+            pluginName={this.props.pluginName}
+            setting={key}
+            Input={<InputComponent {...this.props} />}
+          />
+        </Form.Field>
+      );
+    });
     return <Form>{pluginConfig}</Form>;
   }
 }
