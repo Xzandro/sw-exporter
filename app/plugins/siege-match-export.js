5e758e8a9b2d62b3d817ac04e1a93ff1b5651eb7
levysaar <saarlevy@gmail.com>
2019-02-22 01:20:38 +0200

    catch both attack and defense logs. changed file output name

--- app/plugins/siege-match-export.js
+++ app/plugins/siege-match-export.js
@@ -8,32 +8,32 @@ module.exports = {
   },
   pluginName: 'SiegeMatchExport',
   pluginDescription: 'Exports siege match info.',
-  data: { match: {}, attack: {} },
+  data: {},
   init(proxy, config) {
     proxy.on('GetGuildSiegeMatchupInfo', (req, resp) => {
       if (config.Config.Plugins[this.pluginName].enabled) {
-        this.data.match = resp;
+        this.data['wizard_id'] = req.wizard_id;
+        this.data['matchup_info'] = resp;
         this.writeSiegeMatchToFile(proxy, req, this.data);
       }
     });
     proxy.on('GetGuildSiegeBattleLog', (req, resp) => {
       if (config.Config.Plugins[this.pluginName].enabled) {
-        this.data.attack = resp;
+        req.log_type === 1 ? (this.data['attack_log'] = resp) : (this.data['defense_log'] = resp);
         this.writeSiegeMatchToFile(proxy, req, this.data);
       }
     });
   },
-  writeSiegeMatchToFile(proxy, req, resp) {
-    const wizardID = req.wizard_id;
-    const siegeID = req.siege_id;
-    const filename = sanitize(`SiegeMatch-${wizardID}-${siegeID}`).concat('.json');
+  writeSiegeMatchToFile(proxy, req, data) {
+    const matchID = data.matchup_info.match_info.match_id;
+    const filename = sanitize(`SiegeMatch-${matchID}`).concat('.json');
 
     const outFile = fs.createWriteStream(path.join(config.Config.App.filesPath, filename), {
       flags: 'w',
       autoClose: true
     });
 
-    outFile.write(JSON.stringify(resp, true, 2));
+    outFile.write(JSON.stringify(data, true, 2));
     outFile.end();
     proxy.log({ type: 'success', source: 'plugin', name: this.pluginName, message: `Saved siege match data to file ${filename}` });
   }
