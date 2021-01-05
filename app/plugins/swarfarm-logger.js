6e0f09bcf0163f75c07f853f72223bd91ce6d1e6
QuatZo <48104274+QuatZo@users.noreply.github.com>
2021-01-05 23:31:55 +0100

    Change Swarfarm sync to server-side commands

--- app/plugins/swarfarm-logger.js
+++ app/plugins/swarfarm-logger.js
@@ -2,7 +2,7 @@ const requestLib = require('request');
 
 const SWARFARM_URL = 'https://swarfarm.com/api/v2/';
 const pluginName = 'SwarfarmLogger';
-let request, acceptedCommands, proxy, apiKey;
+let request, acceptedLogCommands, acceptedSyncCommands, proxy, apiKey;
 
 const trimApiKey = inputApiObject => {
   // Trims whitespace from input API key. Only works for plain string input.
@@ -48,7 +48,7 @@ const setRequestAuth = (opts, wizardId) => {
   return opts;
 };
 
-const getApiCommands = () => {
+const getLogApiCommands = () => {
   // Get list of commands from server
 
   proxy.log({
@@ -60,13 +60,13 @@ const getApiCommands = () => {
 
   request.get('data_logs/', (error, response, body) => {
     if (!error && response.statusCode === 200) {
-      acceptedCommands = JSON.parse(body);
+      acceptedLogCommands = JSON.parse(body);
 
       proxy.log({
         type: 'success',
         source: 'plugin',
         name: pluginName,
-        message: `Looking for the following commands to log: ${Object.keys(acceptedCommands)
+        message: `Looking for the following commands to log: ${Object.keys(acceptedLogCommands)
           .filter(cmd => cmd != '__version')
           .join(', ')}`
       });
@@ -81,10 +81,45 @@ const getApiCommands = () => {
   });
 };
 
+const getSyncApiCommands = () => {
+  // Get list of profile sync commands from server
+
+  if (config.Config.Plugins[pluginName].profileSync) {
+    proxy.log({
+      type: 'debug',
+      source: 'plugin',
+      name: pluginName,
+      message: 'Retrieving list of accepted commands for profile synchronization from SWARFARM...'
+    });
+
+    request.get('profiles/accepted-commands/', (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        acceptedSyncCommands = JSON.parse(body);
+
+        proxy.log({
+          type: 'success',
+          source: 'plugin',
+          name: pluginName,
+          message: `Looking for the following commands to sync with your profile: ${Object.keys(acceptedSyncCommands)
+            .filter(cmd => cmd != '__version')
+            .join(', ')}`
+        });
+      } else {
+        proxy.log({
+          type: 'error',
+          source: 'plugin',
+          name: pluginName,
+          message: `Error while getting commands to sync with your profile: ${response.statusCode}`
+        });
+      }
+    });
+  }
+};
+
 const processLog = (req, resp) => {
   const { command, wizard_id } = req;
 
-  if (!acceptedCommands[command]) {
+  if (!acceptedLogCommands[command]) {
     // Not listening for this API command
     return;
   }
@@ -92,7 +127,7 @@ const processLog = (req, resp) => {
   const req_options = setRequestAuth(
     {
       json: true,
-      body: { data: { request: req, response: resp, __version: acceptedCommands.__version } }
+      body: { data: { request: req, response: resp, __version: acceptedLogCommands.__version } }
     },
     wizard_id
   );
@@ -137,127 +172,70 @@ const processLog = (req, resp) => {
 
     // Check if server indicates accepted API commands is out of date
     if (body && body.reinit) {
-      getApiCommands();
+      getLogApiCommands();
     }
   });
 };
 
-const uploadProfile = (req, resp) => {
-  const { command } = req;
-  const { wizard_id } = resp;
+const processSync = (req, resp) => {
+  const { command, wizard_id } = req;
 
-  if (command === 'HubUserLogin') {
-    // Check that API key is filled in
-    if (!apiKey) {
+  if (!acceptedSyncCommands[command]) {
+    // Not listening for this API command
+    return;
+  }
+
+  const req_options = setRequestAuth(
+    {
+      json: true,
+      body: { data: { request: req, response: resp, __version: acceptedSyncCommands.__version } }
+    },
+    wizard_id
+  );
+
+  // Send it
+  request.post('profiles/sync/', req_options, (error, response, body) => {
+    if (error) {
       proxy.log({
         type: 'error',
         source: 'plugin',
         name: pluginName,
-        message: `Profile upload is enabled, but missing API key. Check ${pluginName} settings.`
+        message: `Error: ${error.message}`
       });
-
       return;
     }
 
-    proxy.log({
-      type: 'info',
-      source: 'plugin',
-      name: pluginName,
-      message: 'Uploading profile to SWARFARM...'
-    });
-
-    const req_options = setRequestAuth(
-      {
-        json: true,
-        body: resp
-      },
-      wizard_id
-    );
-
-    request.post('profiles/upload/', req_options, (error, response, body) => {
-      if (error) {
+    // Log message to proxy window
+    if (response.statusCode === 200) {
+      proxy.log({
+        type: 'success',
+        source: 'plugin',
+        name: pluginName,
+        message: `${command} synced successfully`
+      });
+    } else {
+      if (response.statusCode == 401) {
         proxy.log({
           type: 'error',
           source: 'plugin',
           name: pluginName,
-          message: `Error: ${error.message}`
+          message: `SWARFARM Authentication failure: ${body.detail}`
         });
-        return;
-      }
-
-      if (response.statusCode === 200) {
-        proxy.log({
-          type: 'debug',
-          source: 'plugin',
-          name: pluginName,
-          message: 'SWARFARM profile successfully uploaded - awaiting import queue.'
-        });
-
-        const jobId = body.job_id;
-        let resultCheckTimer;
-
-        // 5 minute failsafe to stop checking job status
-        const failsafeCheckTimer = setTimeout(() => {
-          proxy.log({
-            type: 'error',
-            source: 'plugin',
-            name: pluginName,
-            message: 'Timed out retrieving import status.'
-          });
-          clearInterval(resultCheckTimer);
-        }, 300000);
-
-        resultCheckTimer = setInterval(() => {
-          request.get(
-            `profiles/upload/${jobId}/`,
-            setRequestAuth(
-              {
-                json: true
-              },
-              wizard_id
-            ),
-            (resultError, resultResponse, resultBody) => {
-              if (resultBody && resultBody.status === 'SUCCESS') {
-                clearTimeout(failsafeCheckTimer);
-                clearInterval(resultCheckTimer);
-
-                proxy.log({
-                  type: 'success',
-                  source: 'plugin',
-                  name: pluginName,
-                  message: 'SWARFARM profile import complete!'
-                });
-              }
-            }
-          );
-        }, 2500);
-      } else if (response.statusCode === 400) {
-        // HTTP 400 Bad Request - malformed upload data or request
+      } else {
         proxy.log({
           type: 'error',
           source: 'plugin',
           name: pluginName,
-          message: `There were errors importing your SWARFARM profile: ${JSON.stringify(body)}`
-        });
-      } else if (response.statusCode === 401) {
-        // HTTP 401 Unauthorized - Failed to authenticate
-        proxy.log({
-          type: 'error',
-          source: 'plugin',
-          name: pluginName,
-          message: 'Unable to authenticate to SWARFARM. Check your API key in settings.'
-        });
-      } else if (response.statusCode === 409) {
-        // HTTP 409 Conflict - Validation checks failed
-        proxy.log({
-          type: 'error',
-          source: 'plugin',
-          name: pluginName,
-          message: `${JSON.stringify(body)}. You must manually upload your profile on SWARFARM to resolve this.`
+          message: `Error ${response.statusCode}: ${body.detail}`
         });
       }
-    });
-  }
+    }
+
+    // Check if server indicates accepted API commands is out of date
+    if (body && body.reinit) {
+      getSyncApiCommands();
+    }
+  });
 };
 
 module.exports = {
@@ -301,7 +279,7 @@ module.exports = {
       }
 
       try {
-        getApiCommands();
+        getLogApiCommands();
       } catch (error) {
         proxy.log({
           type: 'error',
@@ -310,7 +288,19 @@ module.exports = {
           message: 'Unable to retrieve accepted log types. SWARFARM logging is disabled.'
         });
       }
-      setInterval(getApiCommands, 60 * 60 * 1000); // Refresh API commands every hour
+      setInterval(getLogApiCommands, 60 * 60 * 1000); // Refresh API commands every hour
+
+      try {
+        getSyncApiCommands();
+      } catch (error) {
+        proxy.log({
+          type: 'error',
+          source: 'plugin',
+          name: pluginName,
+          message: 'Unable to retrieve accepted commands for profile synchronization. SWARFARM synchronization is disabled.'
+        });
+      }
+      setInterval(getSyncApiCommands, 60 * 60 * 1000); // Refresh API commands every hour
 
       proxy.on('apiCommand', (req, resp) => {
         // Update API key from settings each log event in case it changed
@@ -328,8 +318,17 @@ module.exports = {
         }
 
         // Profile sync if enabled
-        if (config.Config.Plugins[pluginName].profileSync) {
-          uploadProfile(req, resp);
+        if (config.Config.Plugins[pluginName].profileSync && apiKey) {
+          try {
+            processSync(req, resp);
+          } catch (error) {
+            proxy.log({
+              type: 'error',
+              source: 'plugin',
+              name: pluginName,
+              message: 'Error while synchronizing profile with SWARFARM.'
+            });
+          }
         }
       });
     }
