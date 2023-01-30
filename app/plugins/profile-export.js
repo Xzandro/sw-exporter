2ff6a588cfc5e29df2fb4ee674ecad03484499e5
EytanMorg <jb2947@gmail.com>
2023-01-30 23:00:53 +0100

    added timestamps to exported profile.json

--- app/plugins/profile-export.js
+++ app/plugins/profile-export.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const sanitize = require('sanitize-filename');
+const dateformat = require("date-fns")
 const MISSING_DATA_ERROR =
   'No file created. Data was missing during the Export process. This happens sometimes, when com2us failes to include important data during the request. Normally this fixes itself after a few tries.';
 
@@ -71,8 +72,9 @@ module.exports = {
     });
   },
   writeProfileToFile(proxy, wizardID) {
+    const timestamp = dateformat.format(new Date(), "yyyy-MM-dd_HHmmss");
     const wizardName = this.temp[wizardID].wizard_info.wizard_name;
-    const filename = sanitize(`${wizardName}-${wizardID}`).concat('.json');
+    const filename = sanitize(`${wizardName}-${wizardID}-${timestamp}`).concat('.json');
 
     let outFile = fs.createWriteStream(path.join(config.Config.App.filesPath, filename), {
       flags: 'w',
